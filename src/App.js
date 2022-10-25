fe5c353effd55aeb35223b8dc700e862415ba33f
alex jumbo alava <geossephyjumboalava@gmail.com>
2022-10-25 10:18:13 -0500

    Proyecto con React y Styled Components

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Detail from './components/Detail';
+import Login from './components/Login'
+import {
+  BrowserRouter as Router,
+  Routes, /* Switch */
+  Route,
+  Link
+} from "react-router-dom"; 
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/detail" element={<Detail/>} />
+          <Route path="/" element= {<Home/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
